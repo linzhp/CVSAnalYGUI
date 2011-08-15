521a68416f01bc712041b56448ed2435ec07783c
Zhongpeng Lin <lin.zhp@gmail.com>
2011-08-14 22:52:03 -0700

    user file_pathes as fall back to get current path

--- app/models/action.rb
+++ app/models/action.rb
@@ -10,9 +10,9 @@ class Action < ActiveRecord::Base
   def file_path
     file_path = self.current_file_path
     unless file_path
-      a = Action.where("file_id = ? AND commit_id < ?", self.file_id, self.commit_id).order("commit_id DESC").limit(1)
-      unless a.empty?
-        file_path = a[0].current_file_path
+      fp = FilePath.where("file_id = ? AND commit_id < ?", self.file_id, self.commit_id).order("commit_id DESC").limit(1)
+      unless fp.empty?
+        file_path = fp[0].file_path
         self.current_file_path = file_path
         self.save!
       end
