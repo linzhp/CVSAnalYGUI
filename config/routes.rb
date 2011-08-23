98f3348dc65e12fe3f2cbedb69080596c6238805
Zhongpeng Lin <lin.zhp@gmail.com>
2011-08-22 18:59:11 -0700

    fix a bug of not being able to create users

--- config/routes.rb
+++ config/routes.rb
@@ -62,7 +62,7 @@ Feedback::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "users#index", :as => "users"
+  root :to => "users#index"
 
   # See how all your routes lay out with "rake routes"
 
